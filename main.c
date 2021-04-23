51e1088293185fb1424ec03069e51b539ab41154
Alex <78630640+Alex8442@users.noreply.github.com>
2021-04-23 20:13:44 +0200

    Changed the code in to the english language

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-    //Globale Variablen für Fibonacci
+
     int a=0;
     int b=1;
-    int loesung;
+    int answer;
 
 int main()
 {
@@ -14,18 +14,18 @@ int main()
 
 void start()
 {
-    int auswahl;
-    printf("Hallo, dieses Programm zeigt Rekursive Funktionen\n");
-    printf("Das waren erledigte Aufgaben aus der Schule.\n\n");
-    printf("1) Fibonacci Folge\n");
-    printf("2) Tuerme von Honai\n");
-    scanf("%i",&auswahl);
+    int choice;
+    printf("Hello, this program shows recursive functions\n");
+    printf("This were past school tasks.\n\n");
+    printf("1) Fibonacci numbers\n");
+    printf("2) Towers of honai\n");
+    scanf("%i",&choice);
     system("CLS");
-    if (auswahl==1) {
+    if (choice==1) {
         fibonacci();
     }
-    else if (auswahl==2) {
-    tuerme();
+    else if (choice==2) {
+    towers();
     }
     else {
         start();
@@ -34,43 +34,43 @@ void start()
 
 int fibonacci()
 {
-    int zahl;
+    int number;
 
-    printf("Geben sie eine Zahl ein ");
-    scanf("%i",&zahl);
+    printf("Enter a number ");
+    scanf("%i",&number);
     system("CLS");
     printf("0\n");
-    fibo(zahl-1);
+    fibo(number-1);
     return 0;
 }
 
-int fibo(int zahl)
+int fibo(int number)
 {
-    if (zahl>0)
+    if (number>0)
     {
-    loesung=a + b;
+    answer=a + b;
     a=b;
-    b=loesung;
+    b=answer;
     printf("%i\n",a);
-    fibo(zahl-1);
+    fibo(number-1);
     }
 }
 
-int tuerme()
+int towers()
 {
    int disks;
-    printf("Geben sie die anzahl der disks an: ");
+    printf("Enter the disk amount: ");
     scanf("%i",&disks);
     Spiel(disks,'A','B','C');
     return 0;
 }
 
-void Spiel(int disks,char Links,char Mitte,char Rechts)
+void Spiel(int disks,char left,char middle,char right)
 {
     if(disks>=1)
     {
-        Spiel(disks-1,Links,Rechts,Mitte);
-        printf("%i Disk bewegt %c zu %c\n",disks,Links,Rechts);
-        Spiel(disks-1,Mitte,Links,Rechts);
+        Spiel(disks-1,left,right,middle);
+        printf("%i Disk moves %c to %c\n",disks,left,right);
+        Spiel(disks-1,middle,left,right);
     }
 }
